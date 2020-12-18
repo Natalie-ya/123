b225e06a0b6c6860103f28c2b8ef7b3d186be3ef
Natalie-ya <73675635+Natalie-ya@users.noreply.github.com>
2020-12-18 09:21:20 +0800

    Update settings.py

--- students/settings.py
+++ students/settings.py
@@ -33,11 +33,11 @@ from linebot.models import *
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'Ub8e3cf75739079f25a50f82b2cbd4c63'
+SECRET_KEY = 'd6f491d1-701b-4ceb-abbe-6fd07b74fbdd'
 # 必須放上自己的Channel Access Token
-LINE_CHANNEL_ACCESS_TOKEN ='528gEzSdPX8Pw9mTJlfpXRpTBdETmaT0LJuHs8+3/qBsiz1lWzBRIfWXQexgOdiPyGxeLtL9HAggCjAp0LBJcqKAdv7h8Ux5JQpKMioyobyYoBQpwer/eA3cA1e89/b80sl219M34lvpXlAAFud++wdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_ACCESS_TOKEN ='LGX2DX+AUoFBPGJkX2aJDoWNPkA2qmYksD80US2lSEC0IOGMFwZm+iP1OH7EUg7eS/b9OxCgRLq9CoAQXqST1BXxpXPR01bNx/Zmd5CQfkK3y7CPFWQnJQ/dNyl6jnqGiCRHYMXrsmx/CeUFym48KAdB04t89/1O/w1cDnyilFU='
 # 必須放上自己的Channel Secret
-LINE_CHANNEL_SECRET='43664ae683e1af5eb58087c85b74216d'
+LINE_CHANNEL_SECRET='c011fa7d25ed583acee12e7ef84ed065'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
